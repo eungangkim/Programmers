12affde0bd135ddd53cfe7f05ef60f5c64527f46
eungangkim <105534353+eungangkim@users.noreply.github.com>
2024-10-11 19:31:56 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/README.md
+++ 프로그래머스/2/12911. 다음 큰 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다음 큰 숫자 - 12911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911#) 
 
 ### 성능 요약
 
-메모리: 51.8 MB, 시간: 0.05 ms
+메모리: 51.8 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 19:29:43
+2024년 10월 11일 19:31:55
 
 ### 문제 설명
 
--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,23 +1,13 @@
 class Solution {
-    public int solution(int n) {
-        int tempn=n;
-        int first_one_bit_idx=-1,cnt=-1,beforeBitCount=0;
-        int bitCount=Integer.bitCount(n);
-        
-        while(bitCount==Integer.bitCount(tempn)) {
-        	tempn=tempn>>1;
-        	first_one_bit_idx++;
-        }
-        beforeBitCount=Integer.bitCount(tempn)-1;
-        while(tempn>=0&&beforeBitCount!=Integer.bitCount(tempn)) {
-        	beforeBitCount=Integer.bitCount(tempn);
-        	tempn=tempn>>1;
-        	cnt++;
-        }
-        n+=Math.pow(2,first_one_bit_idx);
-        for(int i=0;i<cnt;i++) {
-        	n+=Math.pow(2,i);
-        }
-        return n;
+    public int  solution(int n)
+    {
+      int a = Integer.bitCount(n);
+      int compare = n+1;
+      while(true) {
+        if(Integer.bitCount(compare)==a)
+          break;
+        compare++;
+      }
+      return compare;
     }
 }
