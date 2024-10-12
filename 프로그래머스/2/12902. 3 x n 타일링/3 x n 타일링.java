1a745aaffe754da29f83e9f1b8ea76c03a742015
eungangkim <105534353+eungangkim@users.noreply.github.com>
2024-10-12 20:01:20 +0900

    [level 2] Title: 3 x n 타일링, Time: 22.80 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -2,10 +2,8 @@ class Solution {
     public int solution(int n) {
 		long []dp=new long[n/2];
 		if(n==2)return 3;
-		else if(n==4)return 11;
 		dp[0]=3;
-		dp[1]=11;
-		for(int i=2;i<n/2;i++) {
+		for(int i=1;i<n/2;i++) {
 			for(int j=0;j<i-1;j++) {
 				dp[i]=(dp[i]+(dp[j]*2)%1000000007)%1000000007;
 			}
