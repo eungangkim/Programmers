77d4cc493253ca1870ba2c52b5d9eeea54e4616e
eungangkim <105534353+eungangkim@users.noreply.github.com>
2024-10-28 17:52:22 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 678.06 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -2,51 +2,51 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
-		int maxMultipleA=getMaxMultiple(arrayA);
-		int maxMultipleB=getMaxMultiple(arrayB);
-		int multipleA=check(arrayB,maxMultipleA);
-		int multipleB=check(arrayA,maxMultipleB);
-		return Math.max(multipleA, multipleB);
-	}
-	public int check(int []array,int maxMultiple) {
-		int multiple=maxMultiple;
-		while(multiple>1) {
-			if(maxMultiple%multiple!=0) {
-				multiple--;
-				continue;
-			}
-			boolean flag=true;
-			for(int i=0;i<array.length;i++) {
-				if(array[i]%multiple==0) {
-					flag=false;
-					break;
-				}
-			}
-			if(flag)break;
-			multiple--;
-		}
-		return multiple==1?0:multiple;
-	}
-	public int getMaxMultiple(int []array) {
-		if(array.length<2)return array[0];
-		
-		int maxMultiple=getMaxMultiple(array[0],array[1]);
-		for(int i=2;i<array.length;i++) {
-			if(array[i]%maxMultiple==0)continue;
-			maxMultiple=getMaxMultiple(maxMultiple,array[i]);
-			if(maxMultiple==1)return 1;
-		}
-		return maxMultiple;
-	}
-	public int getMaxMultiple(int n1,int n2) {
-		int min=Math.min(n1, n2),max=Math.max(n1, n2);
-		
-		for(int i=min;i>=1;i--) {
-			if(max%i==0&&min%i==0) {
-				return i;
-			}
-		}
-		return 1;
-	}
+        int answer = 0;
 
+        int aMin = getMin(arrayA);
+        int aGcd = getArraysGcd(arrayA, aMin);
+
+        int bMin = getMin(arrayB);
+        int bGcd = getArraysGcd(arrayB, bMin);
+
+        answer = getAnswer(aGcd, arrayB, answer);
+        answer = getAnswer(bGcd, arrayA, answer);
+
+        return answer;
+    }
+
+    private static int getAnswer(int gcd, int[] array, int answer) {
+        for (int num : array) {
+            if (num % gcd == 0) {
+                return Math.max(answer, 0);
+            }
+        }
+        return Math.max(answer, gcd);
+    }
+
+    private static int getMin(int[] array) {
+        return Arrays.stream(array)
+                .reduce(Math::min)
+                .getAsInt();
+    }
+
+    private static int getArraysGcd(int[] array, int min) {
+        int gcd = 1;
+        for (int i = 2; i <= min; i++) {
+            if (isGcd(array, i)) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    private static boolean isGcd(int[] array, int i) {
+        for (int num : array) {
+            if (num % i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
