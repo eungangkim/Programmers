5bd3288302f46b15bf89e7994ea45d037bcfd80d
eungangkim <105534353+eungangkim@users.noreply.github.com>
2024-11-25 13:45:48 +0900

    [level 2] Title: 조이스틱, Time: 0.95 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,40 +1,35 @@
 class Solution {
     public int solution(String name) {
-		return dfs(0,1,name.length()-1,name);
+		return dfs(0,0,name.toCharArray());
 	}
-	public int dfs(int idx,int left,int right,String name) {
-		int answer=0;
-		int leftMove=0,rightMove=0;
-		char c=name.charAt(idx);
-		answer+=Math.min((c-'A'),'Z'-c+1 );
-		if(idx==left)
-			left++;
-		if(idx==right)
-			right--;
-		while(left<name.length()&&name.charAt(left)=='A') {
-			left++;
+	public int dfs(int beforeCursor,int cursor,char []nameArr) {
+		if(isDone(nameArr))return 0;
+		int count=nameArr[cursor]<='N'?nameArr[cursor]-'A':91-nameArr[cursor];
+		int big=Math.max(cursor, beforeCursor),small=Math.min(cursor, beforeCursor);
+		count+=Math.min(big-small, small+nameArr.length-big);
+		nameArr[cursor]='A';
+		return count+Math.min(dfs(cursor,getNextCursor(cursor,nameArr,1),nameArr.clone()),dfs(cursor,getNextCursor(cursor,nameArr,-1),nameArr.clone()));
+	}
+	public int getNextCursor(int cursor,char []nameArr,int direction) {
+		int count=0;
+		while(count<nameArr.length&&nameArr[cursor]=='A') {
+			count++;
+			cursor+=direction;
+			if(cursor<0) {
+				cursor=nameArr.length-1;
+			}
+			else if(cursor>=nameArr.length) {
+				cursor=0;
+			}
 		}
-		while(right>=0&&name.charAt(right)=='A') {
-			right--;
+		if(count==nameArr.length)
+			return 0;
+		return cursor;
+	}
+    public boolean isDone(char []nameArr) {
+		for(char c:nameArr) {
+			if(c!='A')return false;
 		}
-        if(left>right)return answer;;
-        if(idx<left) {
-        	rightMove=left-idx;
-           	leftMove=name.length()-right+idx;
-        }
-        else {
-        	rightMove=left+name.length()-idx;
-           	leftMove=idx-right;
-        }
-        int rightMoveAnswer=dfs(left,left,right,name)+rightMove;
-        int leftMoveAnswer=dfs(right,left,right,name)+leftMove;
-        if(rightMoveAnswer<leftMoveAnswer) {
-        	answer+=rightMoveAnswer;
-        }
-        else {
-        	answer+=leftMoveAnswer;
-        }
-        
-		return answer;
+		return true;
 	}
 }
