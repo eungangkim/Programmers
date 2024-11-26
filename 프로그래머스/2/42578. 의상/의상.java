dda7cee10fa0b4ed5b693beec2cc0ac1112bfad7
eungangkim <105534353+eungangkim@users.noreply.github.com>
2024-11-26 17:13:59 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+class Solution {
+   public int solution(String[][] clothes) {
+		HashMap<String,Integer> map=new HashMap<>();
+		int answer=1;
+		
+		for(int i=0;i<clothes.length;i++) {
+			map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0)+1);
+		}
+		
+		for(Entry<String, Integer> x:map.entrySet()) {
+			answer*=(x.getValue()+1);
+		}
+		return answer-1;
+	}
+}
