5851c4395dcbc6a1aa758920ab99027806c894b2
eungangkim <105534353+eungangkim@users.noreply.github.com>
2024-11-27 18:38:11 +0900

    [level 2] Title: N-Queen, Time: 373.87 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,49 +1,39 @@
 class Solution {
     public int solution(int n) {
-        int [][] board=new int[n][n];  
-        return dfs(0,board);
-    }
-	public int dfs(int h,int [][] board) {
-		if(h==board.length) {
+		int [][]board=new int[n][n];
+		return dfs(0,board);
+	}
+	public int dfs(int k,int [][]board) {
+		int cnt=0;
+		int dc[]= {-1,0,1};
+		if(k==board.length) {
 			return 1;
 		}
-		int cnt=0;
 		for(int i=0;i<board.length;i++) {
-			if(board[h][i]==0) {
-				int clone[][]=attackMark(h,i,board);
-				cnt+=dfs(h+1,clone);
-				attackDelete(h,i,board);
+			boolean flag=true;
+			for(int j=0;j<3&&flag;j++) {
+				int r=k,c=i;
+				while(0<=r&&0<=c&&c<board.length&&board[r][c]==0) {
+					r-=1;
+					c+=dc[j];
+				}
+				if(0<=r&&0<=c&&c<board.length)flag=false;
+			}
+			if(flag) {
+				board[k][i]=1;
+				cnt+=dfs(k+1,board);
+                board[k][i]=0;
 			}
 		}
 		return cnt;
 	}
-	public int[][] attackMark(int r,int c,int board[][]){
-		int []dr= {-1,-1, 0, 1,1, 1, 0,-1};
-		int []dc= {0 , 1, 1, 1,0,-1,-1,-1};
-		int ds=0;
-		while(ds<8) {
-			int tr=r,tc=c;
-			while(0<=tr&&tr<board.length&&0<=tc&&tc<board.length) {
-				board[tr][tc]+=1;
-				tr+=dr[ds];
-				tc+=dc[ds];
-			}
-			ds++;
-		}
-		return board.clone();
-	}
-	public void attackDelete(int r,int c,int[][]board) {
-		int []dr= {-1,-1, 0, 1,1, 1, 0,-1};
-		int []dc= {0 , 1, 1, 1,0,-1,-1,-1};
-		int ds=0;
-		while(ds<8) {
-			int tr=r,tc=c;
-			while(0<=tr&&tr<board.length&&0<=tc&&tc<board.length) {
-				board[tr][tc]-=1;
-				tr+=dr[ds];
-				tc+=dc[ds];
-			}
-			ds++;
-		}
-	}
+    public void show(int[][] board){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
