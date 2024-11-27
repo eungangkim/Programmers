0445a7e127bd58bb3c13cbf5a00cbd0d563b135c
eungangkim <105534353+eungangkim@users.noreply.github.com>
2024-11-27 15:42:14 +0900

    [level 2] Title: [1차] 캐시, Time: 42.33 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayDeque;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+		ArrayDeque<String >queue=new ArrayDeque<>();
+		int totalTime=0;
+		for(int i=0;i<cities.length;i++) {
+			cities[i]=cities[i].toLowerCase();
+			if(queue.contains(cities[i])) {
+				queue.remove(cities[i]);
+				queue.add(cities[i]);
+				totalTime++;
+			}
+			else if(cacheSize!=0){
+				if(queue.size()>=cacheSize) {
+					queue.poll();
+				}
+				queue.add(cities[i]);
+				totalTime+=5;
+			}
+            else{
+                totalTime+=5;
+            }
+		}
+		return totalTime;
+	}
+}
