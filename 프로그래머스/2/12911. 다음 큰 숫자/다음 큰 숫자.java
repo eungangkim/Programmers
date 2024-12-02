8da834a47ac8922cb619850419667b7e5085164d
eungangkim <105534353+eungangkim@users.noreply.github.com>
2024-12-02 23:40:05 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.08 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,13 +1,32 @@
-class Solution {
-    public int  solution(int n)
-    {
-      int a = Integer.bitCount(n);
-      int compare = n+1;
-      while(true) {
-        if(Integer.bitCount(compare)==a)
-          break;
-        compare++;
-      }
-      return compare;
-    }
+class Solution{
+    public int solution(int n) {
+		StringBuilder sb=new StringBuilder();
+		String nString=Integer.toBinaryString(n);
+		boolean findOne=false;
+		int countOne=0;
+		for(int i=nString.length()-1;i>=0;i--) {
+			char c=nString.charAt(i);
+			if(findOne&&c=='0') {
+				sb.append(nString.substring(0, i));
+				sb.append("10");
+				for(int j=0;j<nString.length()-i-countOne-1;j++) {
+					sb.append("0");
+				}
+				for(int j=0;j<countOne-1;j++) {
+					sb.append("1");
+				}
+                break;
+			}
+			else if(c=='1') {
+				countOne++;
+				findOne=true;
+			}
+		}
+        if(sb.length()==0) {
+            sb.append("10");
+        	sb.append("0".repeat(nString.length()-countOne));
+        	sb.append("1".repeat(countOne-1));
+        }
+        return Integer.parseInt(sb.toString(),2);
+	}
 }
