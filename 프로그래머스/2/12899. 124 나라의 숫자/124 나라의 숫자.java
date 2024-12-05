3436f93e98385de5346910cefc47105c58e95e85
eungangkim <105534353+eungangkim@users.noreply.github.com>
2024-12-05 17:10:57 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.08 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,17 +1,11 @@
 class Solution {
     public String solution(int n) {
-        StringBuilder sb=new StringBuilder();
-        if(n<=2)return String.valueOf(n);
-        else if(n==3)return "4";
-        
-        if(n%3==0) {
-            sb.append(solution((n-3)/3));
-        	sb.append("4");
-        }
-        else {
-            sb.append(solution(n/3));
-        	sb.append(String.valueOf((n%3)));
-        }
-        return sb.toString();
+		StringBuilder sb=new StringBuilder();
+		char [] arr= {'4','1','2'};
+		while(n>0) {
+			sb.insert(0, arr[n%3]);
+			n=(n-1)/3;
+		}
+		return sb.toString();
 	}
 }
