2533c00032a1b1dcf64ef73a7ad141f4756a8804
eungangkim <105534353+eungangkim@users.noreply.github.com>
2024-12-05 17:39:07 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 12.95 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -1,30 +1,42 @@
+import java.util.ArrayDeque;
+
 class Solution {
     public int[] solution(int m, int n, int[][] picture) {
-        int numberOfArea = 0;
-        int maxSizeOfOneArea = 0;
-        
-        for(int i=0;i<m;i++) {
-        	for(int j=0;j<n;j++) {
-        		if(picture[i][j]>0) {
-        			numberOfArea++;
-        			maxSizeOfOneArea=Math.max(maxSizeOfOneArea,getArea(i,j,picture,picture[i][j]));
-        		}
-        	}
-        }
-        return new int[] {numberOfArea,maxSizeOfOneArea};
-    }
-	public int getArea(int i,int j,int [][]picture,int color) {
-		int cnt=1;
-		int dr[]= {-1,0,1,0};
-		int dc[]= {0,1,0,-1};
-		picture[i][j]=0;
-		for(int x=0;x<4;x++) {
-			int row=i+dr[x];
-			int col=j+dc[x];
-			if(0<=row&&row<picture.length&&0<=col&&col<picture[0].length&&picture[row][col]==color) {
-				cnt+=getArea(row,col,picture,color);
+		int answer[]=new int[2];
+		
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {
+				if(picture[i][j]!=0) {
+					answer[0]++;
+					answer[1]=Math.max(answer[1],bfs(i,j,picture));
+				}
 			}
 		}
-		return cnt;
+		
+		return answer;
+	}
+	public int bfs(int i,int j,int picture[][]) {
+		ArrayDeque<int[]> queue=new ArrayDeque<>();
+		int []dr= {1,0,-1,0};
+		int []dc= {0,1,0,-1};
+		int target=picture[i][j],count=0;
+		
+		queue.add(new int[] {i,j});
+		picture[i][j]=0;
+		
+		while(!queue.isEmpty()) {
+			count++;
+			int info[]=queue.poll();
+			
+			for(int k=0;k<4;k++) {
+				int r=info[0]+dr[k],c=info[1]+dc[k];
+				if(0<=r&&r<picture.length&&0<=c&&c<picture[0].length&&picture[r][c]==target) {
+					picture[r][c]=0;
+					queue.add(new int[] {r,c});
+				}
+			}
+		}
+		
+		return count;
 	}
 }
