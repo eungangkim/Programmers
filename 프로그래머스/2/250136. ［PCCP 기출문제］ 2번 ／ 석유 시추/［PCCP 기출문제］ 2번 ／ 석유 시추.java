ed75b897fd16b81d68638b4d24be73b2bc58d300
eungangkim <105534353+eungangkim@users.noreply.github.com>
2025-11-20 18:01:12 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 70.69 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,60 +1,63 @@
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.ArrayDeque;
+
 class Solution {
-	public int solution(int[][] land) {
-		int answer=0;
-		int k=2;
-		HashMap<Integer,Integer> oilWeights=new HashMap<>();
-		
+		public int solution(int[][] land) {
+		int maxOil=0;
+		int [][] landNum=new int[land.length][land[0].length];	
+		ArrayList<Integer> areas=new ArrayList<>();
+		int nextLandNum=1;
 		for(int i=0;i<land.length;i++) {
 			for(int j=0;j<land[0].length;j++) {
-				if(land[i][j]==1) {
-					oilWeights.put(k, getWeight(k,land,i,j));
-					k++;
+				if(land[i][j]==1&&landNum[i][j]==0) {
+					areas.add(getArea(land,landNum,i,j,nextLandNum));
+					nextLandNum++;
 				}
 			}
 		}
-		for(int j=0;j<land[0].length;j++) {
-			int weight=0;
-			HashSet<Integer> set=new HashSet<>();
-			for(int i=0;i<land.length;i++) {
-				if(land[i][j]!=0&&set.add(land[i][j])) {
-					weight+=oilWeights.get(land[i][j]);
-				}
-			}
-			answer=Math.max(answer, weight);
-        }
-		return answer;
+		
+		for(int k=0;k<land[0].length;k++) {
+			maxOil=Math.max(maxOil, getTotalOil(landNum,areas,k));
+		}
+		
+		return maxOil;
 	}
-	public int getWeight(int k,int [][]land,int i,int j) {
-		int weight=1;
-		ArrayDeque<int []> queue=new ArrayDeque<>();
-		queue.add(new int [] {i,j});
-		land[i][j]=k;
+	private int getArea(int [][]land,int [][] landNum,int row,int col,int nextLandNum) {
+		ArrayDeque<int[]> queue=new ArrayDeque<>();
+		int count=0;
+		int r[]= {-1,0,1,0};
+		int c[]= {0,1,0,-1};
+		landNum[row][col]=nextLandNum;
+		queue.add(new int[] {row,col});
 		while(!queue.isEmpty()) {
-			int point[]=queue.poll();
-			if(point[0]-1>=0&&land[point[0]-1][point[1]]==1) {
-                land[point[0]-1][point[1]]=k;
-    			weight++;
-				queue.add(new int[] {point[0]-1,point[1]});
-			}
-			if(point[0]+1<land.length&&land[point[0]+1][point[1]]==1) {
-                land[point[0]+1][point[1]]=k;
-    			weight++;
-				queue.add(new int[] {point[0]+1,point[1]});	
-			}
-			if(point[1]-1>=0&&land[point[0]][point[1]-1]==1) {
-                land[point[0]][point[1]-1]=k;
-    			weight++;
-				queue.add(new int[] {point[0],point[1]-1});	
-			}
-			if(point[1]+1<land[0].length&&land[point[0]][point[1]+1]==1) {
-                land[point[0]][point[1]+1]=k;
-    			weight++;
-				queue.add(new int[] {point[0],point[1]+1});	
+			count++;
+			int location[]=queue.pop();
+			for(int i=0;i<4;i++) {
+				int nextRow=location[0]+r[i];
+				int nextCol=location[1]+c[i];
+				if(checkRange(nextRow,nextCol,land.length,land[0].length)&&land[nextRow][nextCol]==1&&landNum[nextRow][nextCol]==0) {
+					queue.add(new int[] {nextRow,nextCol});
+					landNum[nextRow][nextCol]=nextLandNum;
+				}
 			}
 		}
-		return weight;
+		return count;
+	}
+	private boolean checkRange(int row,int col,int limitR,int limitC) {
+		if(row>=0&&row<limitR&&col>=0&&col<limitC)
+			return true;
+		return false;
+	}
+	private int getTotalOil(int [][]landNum,ArrayList<Integer> areas,int col) {
+		int totalOil=0;
+		boolean[] visited=new boolean[areas.size()];
+		for(int i=0;i<landNum.length;i++) {
+			int num=landNum[i][col];
+			if(num!=0&&!visited[num-1]) {
+				totalOil+=areas.get(num-1);
+				visited[num-1]=true;
+			}
+		}
+		return totalOil;
 	}
 }
